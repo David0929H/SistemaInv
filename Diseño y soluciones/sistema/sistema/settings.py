af29ff8dafae7f3cdc7492178d2f952fc338ffb2
David0929H <davidhlorca213@gmail.com>
2025-06-29 01:15:21 -0400

    Update settings.py

--- Diseño y soluciones/sistema/sistema/settings.py
+++ Diseño y soluciones/sistema/sistema/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cz*ngirt5bp1^ehtlu9%du9k%5c=gaa!@t07-oo+4ej%cd87q*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [' * ']
 
 
 # Application definition
@@ -121,6 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
